a04f58a155a5473e485ce8be581287cde4f5fb52
cauetexeira <160753286+cauetexeira@users.noreply.github.com>
2024-04-22 12:10:05 -0300

    Add files via upload

---
+++ ATT_Login/Script/Script.js
@@ -0,0 +1,11 @@
+function verificar() {
+    //Setando as ID's para serem lidas apenas com o comando Const
+    const Nome=document.getElementById("Nome");
+    const Senha=document.getElementById("Senha");
+    if(username != "Senai" || senha != "12345678") {
+        alert("A Senha/Usuário inserido está incorreto.");
+      } else {
+
+        
+    }
+}
